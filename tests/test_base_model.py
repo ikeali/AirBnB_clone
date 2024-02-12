27b49f02edbe5e852516cfe8a13e92c35e4eb82f
ikeali <ikechukwualisigwe01@mail.com>
2024-02-12 07:02:25 -0800

    all

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,39 +1,54 @@
-#!/usr/bin/python3
-"""Defines unittests for models/base_model.py.
-Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
-"""
-import os
-import models
 import unittest
-from datetime import datetime
-from time import sleep
 from models.base_model import BaseModel
+from datetime import datetime
+import json
 
+class TestBaseModel(unittest.TestCase):
 
-class TestBaseModel_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the BaseModel class."""
+    def test_init(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj, BaseModel)
+        self.assertTrue(hasattr(obj, 'id'))
+        self.assertTrue(hasattr(obj, 'created_at'))
+        self.assertTrue(hasattr(obj, 'updated_at'))
+        self.assertIsInstance(obj.created_at, datetime)
+        self.assertIsInstance(obj.updated_at, datetime)
 
-    def test_no_args_instantiates(self):
-        self.assertEqual(BaseModel, type(BaseModel()))
+    def test_str(self):
+        obj = BaseModel()
+        string_representation = "[BaseModel] ({}) {}".format(obj.id, obj.__dict__)
+        self.assertEqual(str(obj), string_representation)
 
-    def test_new_instance_stored_in_objects(self):
-        self.assertIn(BaseModel(), models.storage.all().values())
+    def test_save(self):
+        obj = BaseModel()
+        initial_updated_at = obj.updated_at
+        obj.save()
+        self.assertNotEqual(initial_updated_at, obj.updated_at)
 
-    def test_id_is_public_str(self):
-        self.assertEqual(str, type(BaseModel().id))
+    def test_to_dict(self):
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
 
-    def test_created_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(BaseModel().created_at))
+    def test_to_dict_with_custom_attributes(self):
+        obj = BaseModel()
+        obj.custom_attribute = 'test'
+        obj_dict = obj.to_dict()
+        self.assertEqual(obj_dict['custom_attribute'], 'test')
 
-    def test_updated_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(BaseModel().updated_at))
+    def test_init_with_parameters(self):
+        obj_id = '123'
+        created_at = '2022-01-01T12:00:00.000000'
+        updated_at = '2022-01-02T14:30:00.000000'
 
-    def test_two_models_unique_ids(self):
-        bm1 = BaseModel()
-        bm2 = BaseModel()
-        self.assertNotEqual(bm1.id, bm2.id)
+        obj = BaseModel(id=obj_id, created_at=created_at, updated_at=updated_at)
+        self.assertEqual(obj.id, obj_id)
+        self.assertEqual(obj.created_at, datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%f'))
+        self.assertEqual(obj.updated_at, datetime.strptime(updated_at, '%Y-%m-%dT%H:%M:%S.%f'))
 
+if __name__ == '__main__':
+    unittest.main()
 
