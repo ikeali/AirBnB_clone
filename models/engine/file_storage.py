aece6b2598183c9a369068e1526a02814c1c3173
ikeali <ikechukwualisigwe01@mail.com>
2024-02-12 13:01:38 -0800

    all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,45 +1,41 @@
+#!/usr/bin/python3
+"""Module for FileStorage class"""
 import json
-from os.path import exists
+from models.base_model import BaseModel
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file and deserializes JSON file to instances.
-    """
+    """File storage class"""
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns the dictionary __objects.
-        """
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with key <obj class name>.id.
-        """
+        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes __objects to the JSON file (path: __file_path).
-        """
-        serialized_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w', encoding='utf-8') as file:
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        serialized_objects = {}
+        for key, value in self.__objects.items():
+            serialized_objects[key] = value.to_dict()
+        with open(self.__file_path, mode="w", encoding="utf-8") as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
-        """
-        Deserializes the JSON file to __objects.
-        """
-        if exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
-                data = json.load(file)
-                for key, value in data.items():
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, mode="r", encoding="utf-8") as file:
+                deserialized_objects = json.load(file)
+                for key, value in deserialized_objects.items():
                     class_name, obj_id = key.split('.')
-                    class_instance = globals()[class_name](**value)
-                    self.__objects[key] = class_instance
-
+                    class_name = class_name.strip('"')
+                    obj = eval(class_name)(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
 
