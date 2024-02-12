383849ec0a986bcc85957e5b2d272a7a436587d6
ikeali <ikechukwualisigwe01@mail.com>
2024-02-12 06:58:47 -0800

    all

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,67 @@
 #!/usr/bin/python3
 """
-class BaseModel
+This module contains the BaseModel class.
 """
-import models
-from uuid import uuid4
-from datetime import date
 
+import uuid
+from datetime import datetime
 
 class BaseModel:
-    """ Defines all common attributes/methods for
-    other classes """
+    """
+    The BaseModel class for other classes.
+    """
 
     def __init__(self, *args, **kwargs):
-        """ initializes class """
-        if len(kwargs) > 0:
-            for key, value in kwargs.items:
-                if key == "created_at" or key == "updated_at":
-                    value = date.time.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+        """
+        Initializes a new instance of the BaseModel class.
 
+        Parameters:
+        - id (str): The unique identifier for the instance.
+        - created_at (datetime): The creation timestamp.
+        - updated_at (datetime): The last update timestamp.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the instance.
+
+        Returns:
+        - str: A string representation.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the instance.
+
+        Returns:
+        - dict: A dictionary containing all keys/values of __dict__ of the instance.
+        """
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+        return instance_dict
+
+# Example usage
+if __name__ == "__main__":
+    my_object = BaseModel()
+    print(my_object)
+    my_object.save()
+    print(my_object.to_dict())
 
