cc3b4e87dfd8882938f112c8616cedb5b785c96e
ikeali <ikechukwualisigwe01@mail.com>
2024-02-12 11:31:05 -0800

    all

---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+import json
+from os.path import exists
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        """
+        serialized_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects.
+        """
+        if exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    class_instance = globals()[class_name](**value)
+                    self.__objects[key] = class_instance
+
+
